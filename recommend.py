f718376dba6dc6f35a6b5860048eaab05fa0aba7
Raja Gangopadhya <rgangopadhya@ucla.edu>
2013-07-18 09:03:13 -0700

    set up collaborative filtering section

---
+++ recommend.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+def cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambd):
+		"""
+		Collaborative filtering cost function
+		"""
